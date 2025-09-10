7ea40afc5c2b6d7b084063c59851e816308c648d
feijaoozin <gonardo1234@gmail.com>
2025-09-09 21:59:40 -0300

    feat: iniciando página de mapping

--- src/app/pages/Mapping/page.tsx
+++ src/app/pages/Mapping/page.tsx
@@ -1,7 +1,8 @@
 export default function Mapping() {
   return (
     <div>
-      <h1>Mapping Page</h1>
+      <h1 className="text-2xl mb-3">Mapping Page</h1>
+      <p>Colocar exemplos de cada estilo (se possível com imagens).</p>
     </div>
   );
 }
